9cd3e11244df84044848435b6647159f874175a7
DuckyWucky69 <j.d.poczekaj@gmail.com>
2021-11-20 01:26:22 +0100

    double negatives and double positives work, single negatives left to do

--- main.py
+++ main.py
@@ -12,11 +12,18 @@ quadratic = input("Enter quadratic equation: ")
 quadratic = quadratic.replace("^", "**")
 
 def f(value):
+    value = int(value)
     factors = []
-    for i in range(1, int(value**0.5)+1):
-        if value % i == 0:
-            factors.append((i, value / i))
-    return factors
+    if value > 0:
+        for i in range(1,value+1):
+            if value % i == 0:
+                factors.append((i, value / i))
+        return factors
+    elif value < 0: 
+        for i in range(value,0):
+            if value % i == 0:
+                factors.append((i, value / i))
+        return factors
 
 counts = Counter(quadratic)
 count = 0
@@ -29,14 +36,11 @@ for xaxis in x:
 
 if counts['-'] == 0:
 	quadratic = quadratic.split("+")
-	print("a")
 if counts['-'] == 1:
 	minus_pos = [pos for pos, char in enumerate(quadratic) if char == "-"]
 	quadratic = quadratic.replace("-", "+m").split("+")
-	print("b")
 if counts['-'] == 2:
 	quadratic = quadratic.replace("-", "+m").split("+")
-	print(quadratic)
 
 del quadratic[0]
 
@@ -44,31 +48,47 @@ if 'm' in quadratic[0]:
 	quadratic[0] = quadratic[0].replace("m", "-")
 if 'm' in quadratic[1]:
 	quadratic[1] = quadratic[1].replace("m", "-")
-print(quadratic)
 
-if counts['-'] == 1:
+if counts['-'] == 0:
+	factors = f(int(quadratic[1]))
+elif counts['-'] == 1:
 	factors = f(int((quadratic[1]).replace("-", "")))
-	print(factors)
-	factors = '-'+factors # left of here 17.11.21
-elif counts['-'] == 2 or 0:
-	factors = f(int((quadratic[1]).replace("-", "")))
+	i = 0
+	for fac in factors:
+		while i <= 1:
+			print(fac)
+			print(i)
+			factors[i] = tuple(str(str(fac).replace(fac, '-'+fac)))
+			i +=1
+elif counts['-'] == 2:
+	factors = f(int(quadratic[1]))
 
 for factorpair in factors:
 	if str((eval(str(factorpair).replace(" ", "").replace(",","+")))) == str(quadratic[0]+".0"):
 		found_fpair = str(factorpair).replace("(", "").replace(")", "").replace(" ", "").replace(".0", "").split(",")
-		print(found_fpair)
 
-print(found_fpair)
+	elif str((eval(str(factorpair).replace(" ", "").replace(",","-")))) == str(quadratic[0]+".0"):
+		found_fpair = str(factorpair).replace("(", "").replace(")", "").replace(" ", "").replace(".0", "").split(",")
+
+	if str((eval((str(factorpair).replace(" ", "").replace(",","+").split('+'))[0]+"-"+(str(factorpair).replace(" ", "").replace(",","+").split('+'))[1]))) == str(quadratic[0]+".0"):
+		found_fpair = str(factorpair).replace("(", "").replace(")", "").replace(" ", "").replace(".0", "").split(",")
 
 if counts['-'] == 0:
-	print(f"(x+{found_fpair[0]})(x+{found_fpair[1]})")
+	if found_fpair[0] != found_fpair[1]:
+		print(f"(x+{found_fpair[0]})(x+{found_fpair[1]})")
+	elif found_fpair[0] == found_fpair[1]:
+		print(f"(x+{found_fpair[0]})(x-{found_fpair[1]})")
 elif counts['-'] == 1:
 	if '-' in quadratic[0]:
-		print(f"(x-{found_fpair[0]})(x-{found_fpair[1]})")
+		if '-' not in quadratic[1]:
+			print(f"(x-{found_fpair[0]})(x-{found_fpair[1]})")
 	elif '-' in quadratic[1]:
-		print(f"(x)(x)")
+		if '-' in quadratic[0]:
+			print(f"(x-{found_fpair[0]})(x+{found_fpair[1]})")
 elif counts['-'] == 2:
-	print(f"(x+{found_fpair[0]})(x-{found_fpair[1]})")
-print(str(quadratic) + " - quadratic")
+	if '-' in found_fpair[0]:
+		print(f"(x{found_fpair[0]})(x+{found_fpair[1]})")
+	elif '-' in found_fpair[1]:
+		print(f"(x+{found_fpair[0]})(x-{found_fpair[1]})")
 #plt.plot(x_og, y)
 #plt.show()
