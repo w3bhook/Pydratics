3397677f453e72a957fbf5a59d4d314467f5a4ef
DuckyWucky69 <j.d.poczekaj@gmail.com>
2021-11-20 01:57:25 +0100

    Now somewhat works with single negatives, needs logic work

--- main.py
+++ main.py
@@ -49,29 +49,34 @@ if 'm' in quadratic[0]:
 if 'm' in quadratic[1]:
 	quadratic[1] = quadratic[1].replace("m", "-")
 
-if counts['-'] == 0:
-	factors = f(int(quadratic[1]))
-elif counts['-'] == 1:
-	factors = f(int((quadratic[1]).replace("-", "")))
-	i = 0
-	for fac in factors:
-		while i <= 1:
-			print(fac)
-			print(i)
-			factors[i] = tuple(str(str(fac).replace(fac, '-'+fac)))
-			i +=1
-elif counts['-'] == 2:
-	factors = f(int(quadratic[1]))
+factors = f(int(quadratic[1]))
 
 for factorpair in factors:
+
 	if str((eval(str(factorpair).replace(" ", "").replace(",","+")))) == str(quadratic[0]+".0"):
 		found_fpair = str(factorpair).replace("(", "").replace(")", "").replace(" ", "").replace(".0", "").split(",")
 
 	elif str((eval(str(factorpair).replace(" ", "").replace(",","-")))) == str(quadratic[0]+".0"):
 		found_fpair = str(factorpair).replace("(", "").replace(")", "").replace(" ", "").replace(".0", "").split(",")
 
-	if str((eval((str(factorpair).replace(" ", "").replace(",","+").split('+'))[0]+"-"+(str(factorpair).replace(" ", "").replace(",","+").split('+'))[1]))) == str(quadratic[0]+".0"):
+	elif str((eval((str(factorpair).replace(" ", "").replace(",","+").split('+'))[0]+"-"+(str(factorpair).replace(" ", "").replace(",","+").split('+'))[1]))) == str(quadratic[0]+".0"):
 		found_fpair = str(factorpair).replace("(", "").replace(")", "").replace(" ", "").replace(".0", "").split(",")
+
+		if '-' not in factorpair:
+			if (str((eval(str(factorpair).replace(" ", "").replace(",","+")))) == str(quadratic[0]+".0")) == False:
+				if (str((eval(str(factorpair).replace(" ", "").replace(",","-")))) == str(quadratic[0]+".0")) == False:
+					if (str((eval((str(factorpair).replace(" ", "").replace(",","+").split('+'))[0]+"-"+(str(factorpair).replace(" ", "").replace(",","+").split('+'))[1]))) == str(quadratic[0]+".0")) == False:
+						negative_factorpair = '-'+str(factorpair)
+
+						if str((eval(str(negative_factorpair).replace(" ", "").replace(",","+")))) == str(quadratic[0]+".0"):
+							found_fpair = str(negative_factorpair).replace("(", "").replace(")", "").replace(" ", "").replace(".0", "").split(",")
+
+						elif str((eval(str(negative_factorpair).replace(" ", "").replace(",","-")))) == str(quadratic[0]+".0"):
+							found_fpair = str(negative_factorpair).replace("(", "").replace(")", "").replace(" ", "").replace(".0", "").split(",")
+
+						elif str((eval((str(negative_factorpair).replace(" ", "").replace(",","+").split('+'))[0]+"-"+(str(factorpair).replace(" ", "").replace(",","+").split('+'))[1]))) == str(quadratic[0]+".0"):
+							found_fpair = str(negative_factorpair).replace("(", "").replace(")", "").replace(" ", "").replace(".0", "").split(",")
+
 
 if counts['-'] == 0:
 	if found_fpair[0] != found_fpair[1]:
@@ -81,14 +86,15 @@ if counts['-'] == 0:
 elif counts['-'] == 1:
 	if '-' in quadratic[0]:
 		if '-' not in quadratic[1]:
-			print(f"(x-{found_fpair[0]})(x-{found_fpair[1]})")
+			print(f"(x{found_fpair[0]})(x+{found_fpair[1]})")
 	elif '-' in quadratic[1]:
 		if '-' in quadratic[0]:
-			print(f"(x-{found_fpair[0]})(x+{found_fpair[1]})")
+			print(f"(x{found_fpair[0]})(x+{found_fpair[1]})")
 elif counts['-'] == 2:
 	if '-' in found_fpair[0]:
 		print(f"(x{found_fpair[0]})(x+{found_fpair[1]})")
 	elif '-' in found_fpair[1]:
 		print(f"(x+{found_fpair[0]})(x-{found_fpair[1]})")
+
 #plt.plot(x_og, y)
 #plt.show()
